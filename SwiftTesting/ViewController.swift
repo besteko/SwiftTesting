457b207c26eef51532d46d92dd98f8c05a15fcbf
Beste Kocaoğlu <bestekocaoglu@hotmail.com>
2023-10-15 17:42:29 +0300

    Initial Commit

---
+++ SwiftTesting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftTesting
+//
+//  Created by Beste Kocaoglu on 15.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
