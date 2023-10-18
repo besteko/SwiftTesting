96314000119614a274925b6f19077d2bbf37fbd1
Beste Kocaoğlu <bestekocaoglu@hotmail.com>
2023-10-18 17:02:00 +0300

    first commit

---
+++ SwiftTesting/MathematicFunctions.swift
@@ -0,0 +1,24 @@
+//
+//  MathematicFunctions.swift
+//  SwiftTesting
+//
+//  Created by Beste Kocaoglu on 16.10.2023.
+//
+
+import Foundation
+
+class MathematicFunctions {
+    
+    func addIntegers (x: Int, y:Int) -> Int {
+        return x + y
+    }
+    
+    func multiplyNumbers(x: Int, y:Int) -> Int {
+        return x * y
+    }
+    
+    func dividedIntegers (x: Int, y:Int) -> Int {
+        return x / y
+    }
+    
+}
